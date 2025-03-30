dfdb9ceff56408ae888cb8a0096cc714c4819b1a
MrStinger <arseny555eremin@gmail.com>
2025-03-30 13:32:14 +0300

    Add files via upload

--- MinePong.py
+++ MinePong.py
@@ -51,7 +51,7 @@ class Ball(GameSprite):
             score_r += 1
             self.start()
         if self.rect.x >= 700-self.rect.width:
-            score_r += 1
+            score_l += 1
             self.start()
 
 
@@ -80,6 +80,12 @@ background = transform.scale(image.load('fonh.jpg'), (700,500))
 playerL = Player('mech-Photoroom.png', 5,350,70,150,5)
 playerR = Player('mech-Photoroom.png', 625,350,70,150,5)
 
+btn = GameSprite('btn.png', 200,100,350,200,0)
+
+font.init()
+font_1 = font.SysFont('Arial',36)
+win = ''
+
 ball = Ball('tnt.png', 350-25, 250-25, 80, 80, 5)
 ball.direct[0] = choice([-1,1])
 ball.direct[1] = choice([-1,1])
@@ -94,25 +100,60 @@ score_r = 0
 score_l = 0
 rule = 3
 finish = True
+menu = True
 
 
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if menu:
+        window.blit(background, (0,0))
+        btn.reset()
+        pressed = mouse.get_pressed()
+        pos = mouse.get_pos()
+        if pressed[0]:
+            if btn.rect.collidepoint(pos[0], pos[1]):
+                menu = False
+                finish = False
+                win = ''
+                score_l = 0
+                score_r = 0
+        if win != '':
+            winner = font_1.render('Победил '+win+'!', 1, (255,255,255))
+            window.blit(winner, (150, 350))
+            scr = font_1.render('со счетом '+str(max(score_l,score_r))+':'+str(min(score_l,score_r)), 1, (255,255,255))
+            window.blit(scr, (250,400))
 
-    window.blit(background, (0,0))
+    if not finish:
+        window.blit(background, (0,0))
 
 
-    playerL.updateL()
-    playerL.reset()
+        playerL.updateL()
+        playerL.reset()
+        scr_right = font_1.render(str(score_r), 1, (255,255,255))
+        window.blit(scr_right, (670,10))
 
-    playerR.updateR()
-    playerR.reset()
+        
 
-    ball.update()
-    ball.reset()
+        playerR.updateR()
+        playerR.reset()
+        scr_left = font_1.render(str(score_l), 1, (255,255,255))
+        window.blit(scr_left, (10,10))
 
+
+
+        ball.update()
+        ball.reset()
+
+        if score_l >= 3:
+            win = 'Левый игрок'
+            menu = True
+            finish = True
+        if score_r >= 3:
+            win = 'Правый игрок'
+            menu = True
+            finish = True
 
     clock.tick(fps)
     display.update()
